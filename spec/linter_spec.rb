dc7e783b1d774d00511afb31d63dd5fdcf0ac401
craigs40 <craig.shay40@gmail.com>
2021-04-12 20:54:22 -0400

    Update linter_spec

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe CheckError do
   let(:checking) { CheckError.new }
 
   describe '#trailing_white_space' do
-    it 'detects trailing white space' do
-      str = ' string'
-      expect(str.chars[0]).to eq(' ')
-    end
     it 'returns trailing space error' do
       checking.trailing_white_space
       expect(checking.number_of_errors[0]).to eq(['Line 1 ends with trailing white space.'])
@@ -15,10 +11,6 @@ RSpec.describe CheckError do
   end
 
   describe 'empty_line_extra' do
-    it 'detects an extra empty line' do
-      line = ''
-      expect(line.empty?).to eq(true)
-    end
     it 'returns extra empty line error' do
       checking.empty_line_extra
       expect(checking.number_of_errors[0]).to eq(['Line 2: Extra empty line detected.'])
@@ -26,10 +18,6 @@ RSpec.describe CheckError do
   end
 
   describe '#space_around_operator' do
-    it 'detects space around operators' do
-      str = '1 + 2'
-      expect(str.chars[1, 3]).to eq([' ', '+', ' '])
-    end
     it 'returns space around operator error' do
       checking.space_around_operator
       expect(checking.number_of_errors[0]).to eq(['Line 5: Add space around operators.'])
@@ -37,10 +25,6 @@ RSpec.describe CheckError do
   end
 
   describe '#indent' do
-    it 'detects indentation' do
-      str = ' if'
-      expect(str.chars[0]).to eq(' ')
-    end
     it 'returns indent error' do
       checking.indent
       expect(checking.number_of_errors[0]).to eq(['Line 4 should be indented.'])
@@ -48,10 +32,6 @@ RSpec.describe CheckError do
   end
 
   describe '#empty_line_end' do
-    it 'detects empty lines following the end keyword' do
-      line = ''
-      expect(line.empty?).to eq(true)
-    end
     it 'returns error if there is no empty line after end' do
       checking.empty_line_end
       expect(checking.number_of_errors[0]).to eq(['Line 8: Add empty line after end keyword.'])
